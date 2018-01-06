8b062abacebf9876f7e86dab64f8be2eff97cf12
Scott <scott.tolinski@gmail.com>
2018-01-06 16:42:42 -0700

    updates to latest versions and adds onResetStore

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { ApolloClient } from 'apollo-client';
-import { withClientState } from 'apollo-link-state';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo';
-import merge from 'lodash.merge';
+import React from "react";
+import { render } from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { withClientState } from "apollo-link-state";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import merge from "lodash.merge";
 
-import App from './components/App';
-import todos from './resolvers/todos';
-import visibilityFilter from './resolvers/visibilityFilter';
+import App from "./components/App";
+import todos from "./resolvers/todos";
+import visibilityFilter from "./resolvers/visibilityFilter";
 
 const cache = new InMemoryCache();
 
+const stateLink = withClientState({ ...merge(todos, visibilityFilter), cache });
+
 const client = new ApolloClient({
   cache,
-  link: withClientState({ ...merge(todos, visibilityFilter), cache }),
+  link: stateLink
 });
+
+client.onResetStore(stateLink.writeDefaults);
 
 render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
